9ef739a79c03c317f3dddd9acf832478d4b01c4e
dhruv0003 <dhruvpatel152002@gmail.com>
2023-07-02 13:01:11 +0530

    Editing the header image

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -18,14 +18,14 @@ const MealItem = (props) => {
   };
 
   return (
-    <li>
-      <div className={classes.meal}>
+    <li className={classes.meal}>
+      <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm onAddToCart={addToCartHandler}/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
